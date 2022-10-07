e718c38e735fd6b390fbab84f882f91e41a12fe2
Marcelo Alves <alves-marcelo97@hotmail.com>
2022-10-07 17:50:57 -0300

    first commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,35 @@
+import { Header } from '../../components/Header';
+import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from './styles';
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <PriceHighlight variant='outcome'>- R$ 59,00</PriceHighlight>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+
+
+    </div>
+
+  )
+}
